3174085e2443ada35abc2787bcd9f288f3af91ee
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-06-16 00:24:05 +0200

    Add apple icons sort example

--- examples/index.ts
+++ examples/index.ts
@@ -1,3 +1,4 @@
+import AppleIconsSort from './AppleIconSort';
 import TaskPlanner from './TaskPlanner';
 
 export interface ExampleConfig {
@@ -14,5 +15,11 @@ export const EXAMPLES: Array<ExampleConfig> = [
       'A task planning interface with draggable tasks and time slots',
     route: 'task-planner',
     title: 'Task Planner'
+  },
+  {
+    component: AppleIconsSort,
+    description: 'A sortable grid of Apple icons',
+    route: 'apple-icons-sort',
+    title: 'Apple Icons Sort'
   }
 ];
